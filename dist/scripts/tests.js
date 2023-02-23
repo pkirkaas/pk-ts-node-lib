d54f538685fe71afc8917636ba2c76d6ac6590e7
Paul Kirkaas <pkirkaas@gmail.com>
2023-02-23 21:20:58 +0100

    Added adding ./dist in precommit

---
+++ dist/scripts/tests.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const __1 = require("..");
+console.log("Testing lib");
+const anobj = { a: "b", c: "d" };
+const anarr = [1, 2, 3];
+const astr = "abc";
+let tob = (0, __1.typeOf)(anobj);
+let toa = (0, __1.typeOf)(anarr);
+let tos = (0, __1.typeOf)(astr);
+console.log({ tob, toa, tos });
+//# sourceMappingURL=tests.js.map
