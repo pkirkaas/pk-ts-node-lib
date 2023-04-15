1e7546e74afe2516a65fc87b5b39c524e53b5a6d
Paul Kirkaas <pkirkaas@gmail.com>
2023-04-15 02:59:30 +0200

    Endless tweaks to support esm & cjs

--- dist/esm/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from 'pk-ts-common-lib';
-export * from './node-operations.js';
-export * from './files.js';
-export * from './cliSupport.js';
-export * from './configUtils.js';
-export * from './sass-support.js';
-//# sourceMappingURL=index.d.ts.map
